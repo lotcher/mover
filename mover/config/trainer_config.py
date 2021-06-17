b99f3926d51521fbc8e6e08db83a57bb945da84e
bowaer <cb229435444@outlook.com>
2021-06-17 14:44:09 +0800

    feature: 增加trainer config文件路径检验

--- mover/config/trainer_config.py
+++ mover/config/trainer_config.py
@@ -1,3 +1,4 @@
+import os
 from .config import Config
 
 TRAIN_MODE = 'train'
@@ -6,6 +7,12 @@ TRAIN_MODE = 'train'
 class TrainerConfig(Config):
     _must_params = {'TRAIN_DATA_DIR', 'EVAL_DATA_DIR'}
     MODE = TRAIN_MODE
+
+    @classmethod
+    def init(cls, **kwargs):
+        super().init(**kwargs)
+        if not all([os.path.isfile(getattr(cls, file)) for file in cls._must_params]):
+            raise ValueError(f'初始化提供的{cls._must_params}不是合法的文件路径')
 
     TRAIN_DATA_DIR = ''
     EVAL_DATA_DIR = ''
